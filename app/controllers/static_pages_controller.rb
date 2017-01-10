b08b91d162d42b11d56189446f70e5dc496a969b
Gary <gjackson76@gmail.com>
2017-01-10 13:47:36 -0500

    5.3 created action mailer, launched and tested sendgrid

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -8,9 +8,10 @@ class StaticPagesController < ApplicationController
   @name = params[:name]
   @email = params[:email]
   @message = params[:message]
+  UserMailer.contact_form(@email, @name, @message).deliver_now
   ActionMailer::Base.mail(:from => @email,
       :to => 'gjackson76@me.com',
       :subject => "A new contact form message from #{@name}",
       :body => @message).deliver_now
-end
+  end
 end
--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'gjackson76@me.com'
   layout 'mailer'
 end
