04529ee9d88233bef5368df3ed3d944aaafe1a63
Gary <gjackson76@gmail.com>
2017-01-10 12:31:51 -0500

    5.3 add action mailer and sendgrid for test

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   get 'static_pages/index'
   root 'static_pages#index'
 
+  post 'static_pages/thank_you'
+
   resources :orders, only: [:index, :show, :create, :destroy]
 
 
