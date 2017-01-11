9e1942653b3fed9c26035f21fae787446b38bf52
Gary <gjackson76@gmail.com>
2017-01-11 00:10:12 -0500

    5.5 add authorization

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -9,9 +9,5 @@ class StaticPagesController < ApplicationController
   @email = params[:email]
   @message = params[:message]
   UserMailer.contact_form(@email, @name, @message).deliver_now
-  ActionMailer::Base.mail(:from => @email,
-      :to => 'gjackson76@me.com',
-      :subject => "A new contact form message from #{@name}",
-      :body => @message).deliver_now
   end
 end
